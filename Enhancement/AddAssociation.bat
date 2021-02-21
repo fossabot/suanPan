94a41c27d674df2a9392f064ad4473e5d17a6d46
tlcfem@wonderland <tlcfem@gmail.com>
2021-02-21 19:27:01 +1300

    SUPPORT PORTABLE SUBLIME TEXT

--- Enhancement/AddAssociation.bat
+++ Enhancement/AddAssociation.bat
@@ -14,11 +14,18 @@ ftype suanpanmodel="%program%" "-f" "%%1"
 
 set "program=%program:\=/%"
 
-set "target=%appdata%\Sublime Text 3\Packages\User"
+set "target=%appdata%\Sublime Text 3\Packages\User\"
 
-if exist "%target%" (
-	echo {"cmd":["%program%","-f","$file"],"selector":"source.supan","file_patterns":["*.supan","*.sp"]} > "%~dp0suanPan.sublime-build"
-	xcopy "%~dp0suanPan.sublime*" "%target%"
-)
+if exist "%target%" goto copyfile
+
+set /p "target=Input path contains sublime_text.exe (leave empty to quit): "
+if "%target%"=="" goto byebye
+set "target=%target%\Data\Packages\User\"
+set "target=%target:\=/%"
+
+:copyfile
+
+echo {"cmd":["%program%","-f","$file"],"selector":"source.supan","file_patterns":["*.supan","*.sp"]} > "%~dp0suanPan.sublime-build"
+xcopy "%~dp0suanPan.sublime*" "%target%"
 
 :byebye
